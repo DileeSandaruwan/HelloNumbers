f0006974c6b4f2df0e86eac8e7bb7753b802eb9d
DileeSandaruwan <dileepa19990801@gmail.com>
2022-02-18 15:52:51 -0500

    Get maximum value of given array

--- Hello_Numbers.c
+++ Hello_Numbers.c
@@ -5,3 +5,26 @@ int main() {
     int i, sum = 0;
     //declaring an array
     int numbers[10] = { 10,20,30,40,50,60,70,80,90,100 };
+
+    //to find sum of the array numbers
+    for (i = 0; i < 10; i++) {
+        sum = sum + numbers[i];
+    }
+
+    printf("Sum is %d\n", sum);
+
+
+    //to find maximum number of the array
+    int max = numbers[0];
+    for (i = 0; i < 7; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+
+    printf("Max number is %d\n", max);
+
+
+    return 0;
+
+}
